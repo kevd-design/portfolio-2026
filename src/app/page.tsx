c26f33487600c736671352a7e9bd0f2fe3306bc7
Charles K. Delahunt <kevin.delahunt@kevd.design>
2026-01-22 11:42:57 -0500

    Add logo image and display it on the homepage Moved staticly imported asset to src folder under /assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
+import logo from "../assets/kevd.design-logo.png" 
+
 
 
 export default function Home() {
   return (
     <div>
       <p className="font-regular">Hello, world!</p>
+      <Image 
+        src= {logo}
+        alt="kevd.design logo" 
+      />
     </div>
   );
 }
