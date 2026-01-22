1dc68893045600670b2687cab9922c958fe2060c
Charles K. Delahunt <kevin.delahunt@kevd.design>
2026-01-22 11:23:14 -0500

    Remove unused SVG files and update layout and page components to use Fira Sans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Fira_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+
+const firaSans = Fira_Sans({
+  weight: ["300", "400", "600"],
   subsets: ["latin"],
+  style: ["normal", "italic"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${firaSans.className} antialiased`}
       >
         {children}
       </body>
