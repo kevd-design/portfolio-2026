f68d3f53fb134b36ace5d3b96f19b823dc2dbe7d
Charles K. Delahunt <kevin.delahunt@kevd.design>
2026-01-22 13:46:21 -0500

    Add current page styling and improve appearance

--- src/blocks/Header.tsx
+++ src/blocks/Header.tsx
@@ -1,23 +1,27 @@
-import Image from "next/image";
-import logo from "../assets/kevd.design-logo.png" 
+'use client';
 import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import logo from "../assets/kevd.design-logo.png"
 
 export default function Header() {
+    const pathname = usePathname();
+
     return (
-        <header className="mt-16 sm:flex place-content-between">
-                <div className="content-center max-w-3xs m-auto sm:ml-4">
+        <header className=" sm:flex place-content-between bg-white mt-16 py-2">
+            <div className="content-center max-w-3xs m-auto sm:ml-4">
                 <Link href="/">
-                    <Image 
-                    src= {logo}
-                    alt="kevd.design logo"
-                    preload
+                    <Image
+                        src={logo}
+                        alt="kevd.design logo"
+                        preload
                     />
                 </Link>
-                </div>
-                <nav className="flex gap-8 place-content-center m-4">
-                <Link href="/about" className="content-center text-xl font-light hover:font-bold hover:text-indigo-700">About</Link>
+            </div>
+            <nav className="flex gap-8 place-content-center mt-4 px-4">
+                <Link href="/about" className={`content-center text-xl hover:font-bold hover:text-indigo-700 ${pathname === "/about" ? "font-bold text-indigo-700" : "font-light"}`}>About</Link>
                 <a href="#contact" className="content-center text-xl font-light hover:font-bold hover:text-indigo-700">Contact</a>
-                </nav>
+            </nav>
         </header>
     )
 }
